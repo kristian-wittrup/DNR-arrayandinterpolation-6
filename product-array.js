14b858e7b5cf1da88769c36ff5e683e7daac18d1
kristian-wittrup <kw@easv.dk>
2019-03-08 11:13:27 +0100

    Create product-array.js

---
+++ product-array.js
@@ -0,0 +1,14 @@
+      return {
+          products: [
+          { 
+          id:1, 
+          headline: 'Some Islands', 
+          placement:'Number One', 
+          description:'Whitsunday Island, Whitsunday Islands at the blue sea', 
+          price: '10.000', 
+          imgLink:'http://travelquaz.com/wp-content/uploads/2015/09/top-10-travel-destinations-ucoly-com3.jpg' 
+          },
+          { id:2, headline: 'Really high steel structure', placement:'Number Two', description:'Tall structure, windy at top, really popular', price: '7.000', imgLink:'https://zzzippy.com/application/files/1314/6609/9881/Paris_copy.jpg'},
+          { id:3, headline: 'City with many lightbulbs', placement:'Number Three', description:'Asian city in rapid development, looks pretty', price: '15.000', imgLink:'https://www.lindro.it/wp-content/uploads/2018/02/singapore.jpg' },
+        ],
+      }
